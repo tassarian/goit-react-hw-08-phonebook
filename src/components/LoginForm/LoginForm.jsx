60e9be8073e621f082f138b5b0c4a75b4a44ab24
Vladyslav <122233433+tassarian@users.noreply.github.com>
2023-07-30 18:06:01 +0300

    hw

--- src/components/LoginForm/LoginForm.jsx
+++ src/components/LoginForm/LoginForm.jsx
@@ -1,28 +1,41 @@
 import { useDispatch } from 'react-redux';
 import { loginThunk } from 'redux/auth/operations';
 import { Button, Input, InputTitle, StyledForm } from './LoginForm.styled';
+import { useState } from 'react';
 
 export const LoginForm = () => {
 	const dispatch = useDispatch();
+	const [email, setEmail] = useState('');
+	const [password, setPassword] = useState('');
 
 	const handleSubmit = e => {
 		e.preventDefault();
-		const form = e.target;
 		const userData = {
-			email: form.elements.email.value,
-			password: form.elements.password.value,
+			email,
+			password,
 		};
 		dispatch(loginThunk(userData));
-		form.reset();
+		setEmail('');
+		setPassword('');
 	};
 
 	return (
 		<StyledForm onSubmit={handleSubmit}>
 			<InputTitle htmlFor="email">email</InputTitle>
-			<Input type="email" name="email" />
+			<Input
+				type="email"
+				name="email"
+				value={email}
+				onChange={e => setEmail(e.target.value)}
+			/>
 
 			<InputTitle htmlFor="password">password</InputTitle>
-			<Input type="password" name="password" />
+			<Input
+				type="password"
+				name="password"
+				value={password}
+				onChange={e => setPassword(e.target.value)}
+			/>
 
 			<Button type="submit">Log In</Button>
 		</StyledForm>
--- src/components/RegisterForm/RegiserForm.jsx
+++ src/components/RegisterForm/RegiserForm.jsx
@@ -1,32 +1,52 @@
 import { useDispatch } from 'react-redux';
 import { registerThunk } from 'redux/auth/operations';
 import { Button, Input, InputTitle, StyledForm } from './RegisterForm.styled';
+import { useState } from 'react';
 
 export const RegisterForm = () => {
+	const [email, setEmail] = useState('');
+	const [name, setName] = useState('');
+	const [password, setPassword] = useState('');
 	const dispatch = useDispatch();
 
 	const handleSubmit = e => {
 		e.preventDefault();
-		const form = e.target;
 		const userData = {
-			email: form.elements.email.value,
-			name: form.elements.name.value,
-			password: form.elements.password.value,
+			email,
+			name,
+			password,
 		};
 		dispatch(registerThunk(userData));
-		form.reset();
+		setEmail('');
+		setName('');
+		setPassword('');
 	};
 
 	return (
 		<StyledForm onChange={handleSubmit}>
 			<InputTitle htmlFor="email">email</InputTitle>
-			<Input type="email" name="email" />
+			<Input
+				type="email"
+				name="email"
+				value={email}
+				onChange={e => setEmail(e.target.value)}
+			/>
 
 			<InputTitle htmlFor="name">name</InputTitle>
-			<Input type="text" name="name" />
+			<Input
+				type="text"
+				name="name"
+				value={name}
+				onChange={e => setName(e.target.value)}
+			/>
 
 			<InputTitle htmlFor="password">password</InputTitle>
-			<Input type="password" name="password" />
+			<Input
+				type="password"
+				name="password"
+				value={password}
+				onChange={e => setPassword(e.target.value)}
+			/>
 
 			<Button type="submit">Sign Up</Button>
 		</StyledForm>
